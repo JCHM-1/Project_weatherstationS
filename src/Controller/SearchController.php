29b94f9d65dbf0112fe2124e6a53c7d95f0793d9
jaronoun <58861137+jaronoun@users.noreply.github.com>
2022-04-18 20:14:53 +0200

    resp

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -20,7 +20,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use phpDocumentor\Reflection\Type;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
